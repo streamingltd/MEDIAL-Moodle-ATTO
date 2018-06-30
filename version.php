db9a6668c35649428ba4982e83c695eefbedfdb6
Tim Williams <tmw@autotrain.org>
2018-06-30 13:06:26 +0100

    Release version 2018090708-fix

--- version.php
+++ version.php
@@ -24,13 +24,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018090707;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018090708;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;        // Requires this Moodle version.
 $plugin->component = 'atto_helixatto';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
 // Human readable version information
-$plugin->release   = 2018090707;
+$plugin->release   = 2018090708;
 
 $plugin->dependencies = array(
-    'mod_helixmedia' => '2018090707'
+    'mod_helixmedia' => '2018090708'
 );
